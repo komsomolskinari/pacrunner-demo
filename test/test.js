61fc87eb375d6d0a3cbddc2679a9a556e66b3fe5
Student Main <scncgz@gmail.com>
2019-11-02 14:51:16 +0800

    v0.4.0

--- test/test.js
+++ test/test.js
@@ -1,24 +1,27 @@
-var { InitPACFromFile, RunPAC } = require("pacrunner");
+var { PAC } = require("pacrunner");
 
 describe("demo", () => {
     it("this will work", () => {
-        InitPACFromFile("example.pac");
-        const result = RunPAC("https://exhentai.org/img/kokomade.jpg", {
-            __RULES__: ["exhentai.org"],
-            __USERRULES__: [],
-            __PROXY__: "SOCKS 127.0.0.1:1080;"
-        });
+        const result = PAC.FromFile("example.pac").Run(
+            "https://exhentai.org/img/kokomade.jpg",
+            {
+                __RULES__: ["exhentai.org"],
+                __USERRULES__: [],
+                __PROXY__: "SOCKS 127.0.0.1:1080;"
+            }
+        );
         expect(result).toBe("SOCKS 127.0.0.1:1080;");
     });
 
     it("this wont", () => {
-        InitPACFromFile("example.pac");
-        const result = RunPAC("https://exhentai.org/img/kokomade.jpg", {
-            __RULES__: ["google.com"],
-            __USERRULES__: [],
-            __PROXY__: "SOCKS 127.0.0.1:1080;"
-        });
+        const result = PAC.FromFile("example.pac").Run(
+            "https://exhentai.org/img/kokomade.jpg",
+            {
+                __RULES__: ["google.com"],
+                __USERRULES__: [],
+                __PROXY__: "SOCKS 127.0.0.1:1080;"
+            }
+        );
         expect(result).toBe("SOCKS 127.0.0.1:1080;");
     });
-
 });
